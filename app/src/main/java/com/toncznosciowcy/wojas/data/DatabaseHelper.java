be63a2909db6310b26af7484fdb2a148ace61276
KocykNs <pawelkocyk@gmail.com>
2015-03-18 21:52:24 +0100

    schemat bazy

--- app/src/main/java/com/toncznosciowcy/wojas/data/DatabaseHelper.java
+++ app/src/main/java/com/toncznosciowcy/wojas/data/DatabaseHelper.java
@@ -28,8 +28,57 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         // creating required tables
-        db.execSQL(categoryModel.getCreateSQL());
+
+        db.execSQL("CREATE TABLE categories (\n" +
+                "    cat_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                "    cat_name TEXT NOT NULL,\n" +
+                "    cat_picture BLOB NOT NULL,\n" +
+                "    cat_type TEXT NOT NULL\n" +
+                ");\n" +
+                "CREATE TABLE categories_list (\n" +
+                "    cli_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                "    cli_fk_cat_id_from INTEGER NOT NULL,\n" +
+                "    cli_fk_cat_id_to  INTEGER NOT NULL,\n" +
+                "    FOREIGN KEY(cli_fk_cat_id_from) REFERENCES categories(cat_id),\n" +
+                "    FOREIGN KEY(cli_fk_cat_id_to) REFERENCES categories(cat_id)\n" +
+                ");\n" +
+                "CREATE TABLE colors (\n" +
+                "    col_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                "    col_name TEXT NOT NULL\n" +
+                ");\n" +
+                "CREATE TABLE pictures (\n" +
+                "    pic_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                "    pic_blob BLOB NOT NULL\n" +
+                ");\n" +
+                "CREATE TABLE sizes (\n" +
+                "    siz_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                "    siz_name TEXT NOT NULL\n" +
+                ");\n" +
+                "CREATE TABLE colors_pictures (\n" +
+                "    cop_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                "    cop_fk_pic_id INTEGER NOT NULL,\n" +
+                "    cop_fk_col_id INTEGER NOT NULL,\n" +
+                "    FOREIGN KEY(cop_fk_pic_id) REFERENCES pictures(pic_id),\n" +
+                "    FOREIGN KEY(cop_fk_col_id) REFERENCES colors(col_id)\n" +
+                ");\n" +
+                "CREATE TABLE products (\n" +
+                "    prd_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                "    prd_fk_cat_id INTEGER NOT NULL,\n" +
+                "    prd_fk_size_id  INTEGER NOT NULL,\n" +
+                "    prd_name TEXT NOT NULL,\n" +
+                "    prd_value INTEGER NOT NULL,\n" +
+                "    FOREIGN KEY(prd_fk_cat_id) REFERENCES categories(cat_id),\n" +
+                "    FOREIGN KEY(prd_fk_size_id) REFERENCES sizes(siz_id)\n" +
+                ");\n" +
+                "CREATE TABLE products_colors (\n" +
+                "    prc_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                "    prc_fk_prd_id INTEGER NOT NULL,\n" +
+                "    prc_fk_col_id INTEGER NOT NULL,\n" +
+                "    FOREIGN KEY(prc_fk_prd_id) REFERENCES products(prd_id),\n" +
+                "    FOREIGN KEY(prc_fk_col_id) REFERENCES colors(col_id)\n" +
+                ");");
     }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
